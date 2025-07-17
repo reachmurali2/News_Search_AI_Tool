5234f6baa4413a26b9f93a066f539507c84de05e
MURALI KRISHNA REDDY B <125289409+reachmurali2@users.noreply.github.com>
2025-07-17 15:47:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import os
 import streamlit as st
-from dotenv import load_dotenv 
 from rag import process_urls, generate_answer
 
-st.title("News Search Tool")
+st.title("Real Estate Research Tool")
 
 url1 = st.sidebar.text_input("URL 1")
 url2 = st.sidebar.text_input("URL 2")
@@ -15,24 +13,13 @@ url3 = st.sidebar.text_input("URL 3")
 
 placeholder = st.empty()
 
-# Dual secret loading: Streamlit secrets or .env
-
-api_key = st.secrets.get("GROQ_API_KEY")
-
-if api_key is None:
-    load_dotenv()
-    api_key = os.getenv("GROQ_API_KEY")
-
-if api_key is None:
-    st.error("GROQ_API_KEY not found. Please set it in Streamlit secrets or .env file.")
-    
 process_url_button = st.sidebar.button("Process URLs")
 if process_url_button:
     urls = [url for url in (url1, url2, url3) if url!='']
     if len(urls) == 0:
         placeholder.text("You must provide at least one valid url")
     else:
-        for status in process_urls(urls, api_key):
+        for status in process_urls(urls):
             placeholder.text(status)
 
 query = placeholder.text_input("Question")
