4f2e26ee18612dc7b4d5dcb7573dfb4a74e0e52b
MURALI KRISHNA REDDY B <125289409+reachmurali2@users.noreply.github.com>
2025-07-17 15:49:28 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,4 +1,5 @@
-
+# @Author: Dhaval Patel Copyrights Codebasics Inc. and LearnerX Pvt Ltd.
+import os
 from uuid import uuid4                                                 # Generates unique IDs for each document chunk.
 from dotenv import load_dotenv                                         # Loads environment variables (such as API keys) from a .env file.
 from pathlib import Path                                               # Used to manage file system paths (OS independent).
@@ -9,26 +10,28 @@ from langchain_chroma import Chroma                                    # Vector 
 from langchain_huggingface import HuggingFaceEmbeddings                # Loads embedding models from Hugging Face for converting text to vectors.
 from langchain_groq import ChatGroq                                    # Loads Groq-hosted Llama models to generate answers
 
-# from langchain_huggingface.embeddings import HuggingFaceEmbeddings   # Deprecated
-# from langchain.embeddings import HuggingFaceEmbeddings               # Deprecated
-# from langchain.vectorstores import Chroma                            # Deprecated
-# from langchain_community.vectorstores import Chroma                  # Deprecated
-# from langchain_community.embeddings import HuggingFaceEmbeddings     # Deprecated
+# from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+# from langchain.embeddings import HuggingFaceEmbeddings
+
+# from langchain.vectorstores import Chroma
+# from langchain_community.vectorstores import Chroma
+# from langchain_community.embeddings import HuggingFaceEmbeddings
 
 load_dotenv()
 
+api_key=os.getenv("GROQ_API_KEY")                                      # import api_key from .env file
+
 # Constants
 CHUNK_SIZE = 1000                                                      # Max tokens/characters per document chunk
-# CHUNK_OVERLAP = 20
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"             # Pre-trained embedding model
 VECTORSTORE_DIR = Path(__file__).parent / "resources/vectorstore"      # Path to store Chroma database
-COLLECTION_NAME = "News_Assistant"                                     # Name of the vector database collection
+COLLECTION_NAME = "real_estate"                                        # Name of the vector database collection
 
 llm = None
 vector_store = None                                                    # Placeholders for the language model and vector store.
 
 
-def initialize_components(api_key):                                           # Initializes: Groq’s Llama model, Hugging Face embedding model, Chroma vector database
+def initialize_components(api_key):                                     # Initializes: Groq’s Llama model, Hugging Face embedding model, Chroma vector database
     global llm, vector_store
 
     if llm is None:
@@ -37,23 +40,23 @@ def initialize_components(api_key):                                           # 
     if vector_store is None:
         ef = HuggingFaceEmbeddings(
             model_name=EMBEDDING_MODEL,
-            model_kwargs={"trust_remote_code": True}                   # trust_remote_code=True allows the use of custom model loading logic (can be replaced with safer alternatives if needed).
+            model_kwargs={"trust_remote_code": True}          # trust_remote_code=True allows the use of custom model loading logic (can be replaced with safer alternatives if needed).
         )
 
-        vector_store = Chroma(                                         # Initializes a Chroma vector store object. This is where your embeddings and documents will be stored for retrieval.
-            collection_name=COLLECTION_NAME,                           # Names the collection (or namespace) inside the Chroma vector store. Each collection is like a table or bucket that can be queried separately.
-            embedding_function=ef,                                     # Assigns the embedding model that will convert documents and queries into vector representations. In this case, ef is a Hugging Face embedding function.
-            persist_directory=str(VECTORSTORE_DIR)                     # Specifies the path where Chroma will save the vector database to disk. This makes the vector store persistent across sessions (it will be saved locally).
+        vector_store = Chroma(                                # Initializes a Chroma vector store object. This is where your embeddings and documents will be stored for retrieval.
+            collection_name=COLLECTION_NAME,                  # Names the collection (or namespace) inside the Chroma vector store. Each collection is like a table or bucket that can be queried separately.
+            embedding_function=ef,                            # Assigns the embedding model that will convert documents and queries into vector representations. In this case, ef is a Hugging Face embedding function.
+            persist_directory=str(VECTORSTORE_DIR)            # Specifies the path where Chroma will save the vector database to disk. This makes the vector store persistent across sessions (it will be saved locally).
         )
 
 
-def process_urls(urls,api_key):
+def process_urls(urls, api_key):
     """
     This function scraps data from a url and stores it in a vector db
     :param urls: input urls
     :return:
     """
-    yield "Initializing Components...✅"
+    yield "Initializing Components"
     initialize_components(api_key)
 
     yield "Resetting vector store...✅"                      # Clears the existing Chroma collection.
@@ -93,7 +96,10 @@ if __name__ == "__main__":
         "https://www.cnbc.com/2024/12/20/why-mortgage-rates-jumped-despite-fed-interest-rate-cut.html"
     ]
 
-    process_urls(urls)
+    for step in process_urls(urls, api_key):
+        print(api_key)
+
+    # process_urls(urls)
     answer, sources = generate_answer("Tell me what was the 30 year fixed mortagate rate along with the date?")
     print(f"Answer: {answer}")
     print(f"Sources: {sources}")
